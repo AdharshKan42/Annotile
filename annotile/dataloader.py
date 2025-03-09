43d221fa588698627e8bc7d4637f63e3c3070af2
Adharsh Kandula <adharsh.kandula@gmail.com>
2025-03-09 03:04:51 -0400

    Add unmatched annotations to Dataloader and manual testing with mock data

--- annotile/dataloader.py
+++ annotile/dataloader.py
@@ -34,23 +34,38 @@ class Dataloader(BaseModel):
     # These will be automatically filled after validation
     paired: List[Tuple[Path, Path]] = []
     unmatched_images: List[Path] = []
+    unmatched_annotations: List[Path] = []
 
     @model_validator(mode="after")
     def process_files(self):
         annotation_map = {ann.stem: ann for ann in self.annotation_paths}
-        paired = []
-        unmatched = []
+        image_map = {img.stem: img for img in self.image_paths}
 
-        for img in self.image_paths:
-            annotation = annotation_map.get(
-                img.stem
-            )  # Match by filename (without extension)
-            if annotation:
-                paired.append((img, annotation))
-            else:
-                unmatched.append(img)
+        paired = []
+        unmatched_images = []
+        unmatched_annotations = []
+
+        # Check the Set difference between the two sets
+        annotation_set = set(annotation_map.keys()) 
+        image_set = set(image_map.keys())
+        
+        # Annotations without corresponding images
+        unmatched_annotation_stems = annotation_set - image_set
+        if len(unmatched_annotation_stems) > 0:
+            unmatched_annotations = [annotation_map[stem] for stem in unmatched_annotation_stems]
+
+        # Images without corresponding annotations
+        unmatched_image_stems = image_set - annotation_set
+        if len(unmatched_image_stems) > 0:
+            unmatched_images = [image_map[stem] for stem in unmatched_image_stems]
+
+        # Match images with annotations
+        paired_images = image_set.intersection(annotation_set)
+        paired = [(image_map[stem], annotation_map[stem]) for stem in paired_images]
 
         # Assign the processed values to the instance
         self.paired = paired
-        self.unmatched_images = unmatched
+        self.unmatched_images = unmatched_images
+        self.unmatched_annotations = unmatched_annotations
+
         return self
---
+++ annotile/test_dataloader.py
@@ -0,0 +1,16 @@
+from dataloader import Dataloader, get_yolo_annotation_paths,  get_yolo_image_paths
+from pathlib import Path
+
+# Create a path to tests/data
+data_path = (Path(__file__).parent / "../tests/data").resolve()
+print(data_path)
+
+image_paths = get_yolo_image_paths(data_path)
+annotation_paths = get_yolo_annotation_paths(data_path)
+
+dataloader = Dataloader(image_paths=image_paths, annotation_paths=annotation_paths)
+print(dataloader.paired)
+print("**" * 20)
+print(dataloader.unmatched_images)
+print("**" * 20)
+print(dataloader.unmatched_annotations)
